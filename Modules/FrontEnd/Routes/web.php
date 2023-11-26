ebfc856eaef60efb9945a34d54533a4f9a234859
Md Rezaul Karim <rkreza24@gmail.com>
2023-11-26 07:02:26 +0600

    Update web.php

--- Modules/FrontEnd/Routes/web.php
+++ Modules/FrontEnd/Routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware'=>'language'],function (){
     Route::get('/', 		        'FrontEndController@home')->name('frontend.home');
-    Route::get('/set_city', 	    'FrontEndController@set_city')->name('frontend.set_city');
-    Route::get('/get_routes', 	    'FrontEndController@get_routes')->name('frontend.get_routes');
-    Route::get('/get_route_by_id', 	    'FrontEndController@get_route_by_id')->name('frontend.get_route_by_id');
+    Route::get('/delete-user', 	    'FrontEndController@delete_user')->name('frontend.delete_user');
 
 
     // Route::get('/load-ajax_trip_header_blade', 'FrontEndController@load_ajax_trip_header_blade')->name('frontend.load_ajax_trip_header_blade');
