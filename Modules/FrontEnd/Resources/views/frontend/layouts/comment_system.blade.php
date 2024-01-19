937f31f883be61aa39eb5860c997d5e4161bb407
Md Rezaul Karim <rkreza24@gmail.com>
2024-01-19 12:15:11 +0600

    Update comment_system.blade.php

--- Modules/FrontEnd/Resources/views/frontend/layouts/comment_system.blade.php
+++ Modules/FrontEnd/Resources/views/frontend/layouts/comment_system.blade.php
@@ -30,7 +30,7 @@
                             </div>
                         </form>
                     @else
-                        <h4 class="mt-4" id="comment_form">Please <a href="{{ route('admin.login') }}">sign in</a> to write a comment</h4>
+                        <h4 class="mt-4" id="comment_form">S'il te plaît <a href="{{ route('admin.login') }}">se connecter</a> pour écrire un commentaire</h4>
                     @endif
                 </div>
             </div>
