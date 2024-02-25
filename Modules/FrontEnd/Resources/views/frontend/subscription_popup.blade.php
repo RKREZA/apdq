f2b6d8840991be1bc89f7899e01bd51cfca9f920
Md Rezaul Karim <rkreza24@gmail.com>
2024-02-26 05:09:16 +0600

    Update subscription_popup.blade.php

--- Modules/FrontEnd/Resources/views/frontend/subscription_popup.blade.php
+++ Modules/FrontEnd/Resources/views/frontend/subscription_popup.blade.php
@@ -4,19 +4,19 @@
         <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content bg-dark border-0">
             <div class="modal-header border-0">
-            <h5 class="modal-title" id="subscriptionModalLabel">Premium Content Access</h5>
+            <h5 class="modal-title" id="subscriptionModalLabel">Accès au contenu premium</h5>
             <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-            Get exclusive access to our premium content by subscribing now. Don't miss out on our in-depth articles, guides, and more!
+                Obtenez un accès exclusif à notre contenu premium en vous abonnant maintenant. Ne manquez pas nos articles approfondis, nos guides et bien plus encore !
             </div>
             <div class="modal-footer border-0">
                 <div class="row w-100">
                     <div class="col-md-6">
-                        <button type="button" class="btn btn-outline-light w-100 border-radius-round" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-outline-light w-100 border-radius-round" data-bs-dismiss="modal">Fermer</button>
                     </div>
                     <div class="col-md-6">
-                        <a href="{{ route('frontend.subscription') }}" type="button" class="btn btn-primary w-100 border-radius-round">Subscribe Now</a>
+                        <a href="{{ route('frontend.subscription') }}" type="button" class="btn btn-primary w-100 border-radius-round">Abonnez-vous maintenant</a>
                     </div>
                 </div>
             </div>
