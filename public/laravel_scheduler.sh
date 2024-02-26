0be4d0e4830a1b4393baf4e1b2760c66360afff6
Md Rezaul Karim <rkreza24@gmail.com>
2024-02-26 06:50:49 +0600

    Update laravel_scheduler.sh

--- public/laravel_scheduler.sh
+++ public/laravel_scheduler.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/php /home/u933481031/domains/productionjasmindupaul.com/APDQ/artisan schedule:run >> /dev/null 2>&1
+/usr/bin/php /home/u933481031/domains/productionjasmindupaul.com/public_html/APDQ//artisan schedule:run >> /dev/null 2>&1
